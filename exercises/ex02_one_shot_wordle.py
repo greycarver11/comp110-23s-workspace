9c5bbb485653d2a5a85fb2d6bf49047f696db28d
Grey Carver <greymc@email.unc.edu>
2023-02-10 17:07:10 -0500

    2/10 EX02

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,42 @@
+"""EX02 - One-Shot Wordle - Loops!"""
+__author__ = 730394136
+
+SECRET: str = "python"
+guess: str = input("What is your 6-letter guess? ")
+playing: bool = True
+
+idx: int = 0
+emoji: str = ""
+
+GREEN_BOX: str = "\U0001F7E9"
+WHITE_BOX: str = "\U00002B1C"
+YELLOW_BOX: str = "\U0001F7E8"
+
+while len(guess) != len(SECRET):
+    guess = input("That was not 6 letters! Try again: ")
+while playing and idx < len(SECRET):
+    if guess[idx] == SECRET[idx]:
+        emoji = emoji + GREEN_BOX 
+    else:
+        yellow_letter: bool = False
+        idx_2: int = 0
+        while yellow_letter != True and idx_2 < len(SECRET):
+            if SECRET[idx] == guess[idx_2]:
+                yellow_letter = True
+            else:
+                idx_2 = idx_2 + 1
+        if yellow_letter == True:
+            emoji = emoji + YELLOW_BOX
+        if yellow_letter == False:
+            emoji = emoji + WHITE_BOX 
+    idx = idx + 1 
+
+if guess != SECRET:
+    print (emoji)
+    print("Not quite. Play again soon")
+    playing = False
+if guess == SECRET: 
+    print (emoji)
+    print("Woo! You got it!")
+    playing = False
+
---
+++ lessons/mimic.py
@@ -0,0 +1,5 @@
+"""Given the string my_words, outputs the same string"""
+
+my_words: str = "Hello"
+If input = str("mimic")
+    print my_words
